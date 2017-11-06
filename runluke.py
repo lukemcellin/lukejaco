55fa1a5e3ac26dc75f74274ab46c4faecd95bb4b
lukemcellin <33333457+lukemcellin@users.noreply.github.com>
2017-11-06 12:55:25 -0800

    Add files via upload

--- runluke.py
+++ runluke.py
@@ -24,25 +24,22 @@ from velsampling import *
 
 #reverse sign of 1 4,6 7 so animation isn't upside down
 
-buttonpos = np.array([[3.08414056, 2.11856419, 0.0,  -0.61303322,  0.0, -2.73159741, 1.51334423],
- [-2.53191311e+00, 1.74561523e+00, 0.00000000e+00, -2.48885741e+00,  -7.10542736e-15, 2.04871267e+00, 2.18047587e+00],
- [-2.81503678e+00, 1.97147218e+00, 0.00000000e+00, -1.90069778e+00,   -7.10542736e-15, 2.41101535e+00, 1.89735220e+00],
- [2.36385650e-02, -2.02706932e+00, 0.00000000e+00, 1.73870882e+00,
+buttonpos = np.array([[3.08414056, 2.11856419, 0.0/math.pi*2,  -0.61303322,  0.0, -2.73159741, 1.51334423],
+ [-2.53191311e+00, 1.74561523e+00, 0.00000000e+00/math.pi*2, -2.48885741e+00,  -7.10542736e-15, 2.04871267e+00, 2.18047587e+00],
+ [-2.81503678e+00, 1.97147218e+00, 0.00000000e+00/math.pi*2, -1.90069778e+00,   -7.10542736e-15, 2.41101535e+00, 1.89735220e+00],
+ [2.36385650e-02, -2.02706932e+00, 0.00000000e+00/math.pi*2, 1.73870882e+00,
     0.00000000e+00, -2.51740716e+00, -1.54715776e+00],
- [2.77060652e+00, 1.97147218e+00, 0.00000000e+00, -1.90069778e+00,
+ [2.77060652e+00, 1.97147218e+00, 0.00000000e+00/math.pi*2, -1.90069778e+00,
    -7.10542736e-15, 2.41101535e+00, 1.19981020e+00],
- [2.49367962e+00, 1.74561523e+00, 0.00000000e+00, -2.48885741e+00,
+ [2.49367962e+00, 1.74561523e+00, 0.00000000e+00/math.pi*2, -2.48885741e+00,
    -7.10542736e-15, 2.04871267e+00, 9.22883296e-01]])
 
-buttonpos = numpy.mod(np.array(buttonpos),math.pi*2)
+buttonpos = np.mod(np.array(buttonpos),math.pi*2)
 
-
-
-#targpos = buttonpos[:,targetnum]
 
 	#loop for whole sequence
-	#seqtraj = np.array([])
-sequence = [0,5,1]
+seqtraj = np.empty((0,7))
+sequence = [0,1,5,2,4,5,1]
 for index in range(1,len(sequence)):
 	sindex = sequence[index-1]
 	start = buttonpos[sindex,:]
@@ -51,13 +48,12 @@ for index in range(1,len(sequence)):
 
 	if __name__ == '__main__':
 
-		# work out start and end positions
-
-		# set the start and goal pose of the robot in radians
-	#start = np.array(pick)*(math.pi/180.0)
-		#start = np.array([-6.50458581e-02,-2.06374240e+00, 0.00000000e+00,  5.31513413e-01, 7.10542736e-15, -2.59525581e+00, -1.63584218e+00])
-		#goal = np.array(place)*(math.pi/180.0)
-	#goal = np.array([-0.60967954, -1.52711067, 0, 2.66790655, 0,          -2.08816808, -2.18047587])
+		#input relevant stuff
+		veltype = input('1 = no pause, 2 = pause')
+		skew = input('0.5 = symmetry between acel & decel')
+		kurt = input('1-20: sharpness of acel & decel')
+		pausepos = input('from 0-1: when hand will pause')
+		pausetime = input('amount of frames hand will pause')
 
 
 	# create the trajectory optimization-based planner
@@ -65,7 +61,7 @@ for index in range(1,len(sequence)):
 
 	# max table weight = 0, which means robot will prioritize staying close to the table
 	# See what happens when you change it to 1!
-		table_weight = -0.5
+		table_weight = 0
 	
 	# set the start time of the traj, final time and step time (in seconds)
 		start_time = 0.0
@@ -76,22 +72,30 @@ for index in range(1,len(sequence)):
 		#goal = start
 		#start = goal
 		traj = planner.replan(start, goal, table_weight, start_time, final_time, step_time)		
-		#seqtraj = numpy.append(seqtraj,traj)
+
 		
 		#take traj and transform it to desired velocity profile		
-		sampledtraj = velprofile(traj)
-		sampledtraj = np.transpose(sampledtraj)
+		if veltype == 1:
+			sampledtraj = velprofile(traj,skew,kurt)
+			sampledtraj = np.transpose(sampledtraj)
+
+		elif veltype == 2:
+			sampledtraj = velpause(traj,skew,kurt,pausepos,pausetime)
+			sampledtraj = np.transpose(sampledtraj)
+
+
+		seqtraj = np.append(seqtraj,sampledtraj,axis=0)
 
 		# plot the trajectory using functions from openrave_utils
-		plotTraj(planner.env, planner.robot, planner.bodies, sampledtraj, color=[1,0,0]) #, increment=5)
+	plotTraj(planner.env, planner.robot, planner.bodies, sampledtraj, color=[1,0,0]) #, increment=5)
 	
-		print(traj)
-		np.savetxt('jacotraj', np.array(traj), delimiter=",")
+
+		#np.savetxt('jacotraj', np.array(traj), delimiter=",")
 
 
-		# sleep the sim for 20 seconds so the program doens't shut down
-		time.sleep(1)
-		try:
-    			input("Press enter to continue")
-		except SyntaxError:
-    			pass
+	# sleep the sim for 20 seconds so the program doens't shut down
+	time.sleep(1)
+	try:
+    		input("Press enter to continue")
+	except SyntaxError:
+    		pass
--- velsampling.py
+++ velsampling.py
@@ -1,93 +1,102 @@
-#def velprofile(traj,samplingtype,skew,kurt):
-def velprofile(traj):
+import scipy
+import numpy as np
+import skfuzzy
 
-	import scipy
-	import numpy as np
-	import skfuzzy
+## sampling waypoints based on a sigmoid function gives us a bell shaped velocity profile... two parameters allow us to control 'skewness'(symmetry) and 'kurtosis'(sharpness of acceleration and deceleration) of velocity profile
+ 
+def velprofile(traj,skew,kurt):
 
-
-	#jacotraj = np.genfromtxt('jacotraj',delimiter =",")	
-	jacotraj = traj
-	#samples = len(jacotraj)
-	samples = 31
+	samples = len(traj)
 	allframes = 15000 #time in ms (one frame per ms)
 
-# create desired function for velocity profile
+	#create linear function to transform	
+	x = range(1,101,1); 
+	x = np.array(x)
+	x = np.true_divide(x,100)
 
-#sigmoidal function sampling
-	
-	#if samplingtype == 1
-	#x = range(1,101,1); #create linear function to transform
-	#x = np.array(x)
-	#x = np.true_divide(x,100)
-
-	#index = skfuzzy.membership.sigmf(x,skew,kurt)
-	#index = skfuzzy.membership.sigmf(x,0.5,20) #sigmoid for bell shaped velocity
-	#index = index*allframes
-	#index = np.rint(index)
-
-## logit function sampling
-
-	#index = log; #logit for fast movement with slowing in the middle.... maybe invert the sigmoid somehow
-
-
-## double sigmoid sampling for stop and start.... maybe find a smarter way to generate polynomial functions 
-
-	#elif samplingtype == 2
-
-	pausepos = 0.5 #determine at what stage of the action robot will pause
-
-	#before pause
-	x1 = range(1,101,1)
-	x1 = np.true_divide(x1,100)
-	x1 = skfuzzy.membership.sigmf(x1,0.5,15) 
-	size1 = round(len(x1)*pausepos)
-	a = x1
-	b = np.linspace(0,1,len(x1))
-	index1 = np.interp(np.linspace(0,1,size1),b,a)
-	index1 = index1*pausepos
-
-	#after pause
-	x2 = range(1,101,1) 
-	x2 = np.true_divide(x2,100)
-	x2 = skfuzzy.membership.sigmf(x2,0.5,15) 
-	size2 = round(len(x2)*(1-pausepos))
-	a = x2
-	b = np.linspace(0,1,len(x2))
-	index2 = np.interp(np.linspace(0,1,size2),b,a)
-	index2 = index2*(1-pausepos)
-	index2 = index2 + index1[len(index1)-1]
-
-	#add pause
-	pausesize = 2
-	pause = np.empty(pausesize)
-	pause.fill(index1[len(index1)-1])
-
-
-	index = np.concatenate((index1,pause,index2))
+	#transform linear function to sigmoidal function
+	index = skfuzzy.membership.sigmf(x,skew,kurt) 
 	index = index*allframes
 	index = np.rint(index)
 
-### interpolation and sampling
-
+	#interpolate up to 1000Hz (iterate for each joint)
 	interpmatrix = np.empty((0,allframes))
-	#interpolate for one sample every 1ms
-	#iterate for each joint
 	for i in range(0,7):
-		a = jacotraj[:,i]
-		b = np.linspace(0,1,len(jacotraj))
+		a = traj[:,i]
+		b = np.linspace(0,1,len(traj))
 		interptraj = np.interp(np.linspace(0,1,allframes),b,a)
 		interpmatrix = np.append(interpmatrix,[interptraj],axis=0)
-
 	interpmatrix = np.transpose(interpmatrix)
 
-	# interpolate desired amount of samples
-
+	#interpolate sampling array for desired amount of samples
 	a = index
 	b = np.linspace(0,1,len(index))
 	sampling = np.interp(np.linspace(0,1,samples),b,a) #interp samples
 	sampling = np.rint(sampling) # round for indexing
 	sampling = np.int_(sampling)
+
+	#sample from interpolated trajs using sampling distribution
+	sampledtraj = interpmatrix[sampling-1,:]
+	sampledtraj = np.transpose(sampledtraj)
+
+	return(sampledtraj)
+
+
+
+## double sigmoid sampling, allows us to control skewness and kurtosis of velocity profile before and after the pause.... also allows us to control when to pause and for how long....
+
+def velpause(traj,skew,kurt,pausepos,pausetime):
+
+	samples = len(traj)
+	allframes = 15000 #time in ms (one frame per ms)
+
+	#generate sigmoid function for before pause
+	x1 = range(1,101,1)
+	x1 = np.true_divide(x1,100)
+	x1 = skfuzzy.membership.sigmf(x1,skew,kurt) 
+	size1 = round(len(x1)*pausepos)
+	a = x1
+	b = np.linspace(0,1,len(x1))
+	index1 = np.interp(np.linspace(0,1,size1),b,a)#change size based on pausepos
+	index1 = index1*pausepos
+
+	#generate sigmoid function for after pause
+	x2 = range(1,101,1) 
+	x2 = np.true_divide(x2,100)
+	x2 = skfuzzy.membership.sigmf(x2,skew,kurt) 
+	size2 = round(len(x2)*(1-pausepos))
+	a = x2
+	b = np.linspace(0,1,len(x2))
+	index2 = np.interp(np.linspace(0,1,size2),b,a)#change size based on pausepos
+	index2 = index2*(1-pausepos)
+	index2 = index2 + index1[len(index1)-1]
+
+	#add pause
+	pause = np.empty(pausetime)
+	pause.fill(index1[len(index1)-1])
+
+	#concatenate pause with movements before and after pause
+	index = np.concatenate((index1,pause,index2))
+	index = index*allframes
+	index = np.rint(index)
+
+	#interpolate up to 1000Hz (iterate for each joint)
+	interpmatrix = np.empty((0,allframes))
+	for i in range(0,7):
+		a = traj[:,i]
+		b = np.linspace(0,1,len(traj))
+		interptraj = np.interp(np.linspace(0,1,allframes),b,a)
+		interpmatrix = np.append(interpmatrix,[interptraj],axis=0)
+	interpmatrix = np.transpose(interpmatrix)
+
+	#interpolate sampling array for desired amount of samples
+	a = index
+	b = np.linspace(0,1,len(index))
+	sampling = np.interp(np.linspace(0,1,samples),b,a) #interp samples
+	sampling = np.rint(sampling) # round for indexing
+	sampling = np.int_(sampling)
+
+	#sample from interpolated trajs using sampling distribution
 	sampledtraj = interpmatrix[sampling-1,:]
 	sampledtraj = np.transpose(sampledtraj)
 
